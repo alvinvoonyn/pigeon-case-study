5b40faf9805fdd89b3a8890efda973bc7f75059b
alvinvoonyn <alvinvoonyn@yahoo.com>
2018-07-27 20:12:24 +0800

    Added pigeon-auth and owl-carousel library

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@
 /*jslint vars: true*/
 /*jslint plusplus: true*/
 
-var app = angular.module("myApp", ['ngRoute', 'pigeon-table', 'pigeon-chart']);
+var app = angular.module("myApp", ['ngRoute', 'pigeon-auth', 'pigeon-table', 'pigeon-chart']);
 
 app.config(function ($routeProvider) {
 	$routeProvider
@@ -36,7 +36,6 @@ app.controller("dashboardCtrl", function ($scope, $http) {
 
     function getData() {
 
-
         $http.post("pigeon-core/get-data.php", {'sql': "SELECT GENDER as Gender, count(GENDER) as 'Number of Actor' FROM actor GROUP BY GENDER"})
             .then(function(response) {
                 $scope.actor = response.data.data;
@@ -50,15 +49,39 @@ app.controller("dashboardCtrl", function ($scope, $http) {
 		$http.get("https://api.themoviedb.org/3/discover/movie?api_key=40f95d64ec838aed5995fafa9cbecda8&language=en-US&sort_by=release_date.desc&include_adult=false&include_video=false&page=1").then(function(response){
 			angular.forEach(response.data.results, function(obj){
 				if(obj.poster_path != null){
+                    console.log(obj);
 					$scope.images.push(obj.poster_path);
 				}
 			});
-				$(document).ready(function(){
-					 $("#carousel").carousel();
-				});
+
 		});
+
+
+
     }
 
     getData();
 
+});
+
+app.directive('initOwlCarousel', function () {
+    return {
+        restrict: 'A',
+        transclude: false,
+        link: function(scope, element) {
+
+          // wait for the last item in the ng-repeat then call init
+            if(scope.$last) {
+                $(element.parent()).owlCarousel({
+                    items: 1,
+                    loop: true,
+                    margin: 20,
+                    autoplay: true,
+                    autoplayTimeout: 3000,
+                    autoplayHoverPause: true,
+                    autoHeight: false
+                });
+            }
+        }
+    };
 });
